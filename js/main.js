d4884c39436b75c0f13565d12b1a3410a89cdcfc
damiandusinski <dusinski01@gmail.com>
2019-04-22 13:00:48 +0200

    Add timer

--- js/main.js
+++ js/main.js
@@ -1,22 +1,79 @@
 const times = document.querySelectorAll(".time");
 const environments = document.querySelectorAll(".environment");
+const btnStart = document.querySelector(".btn-start");
+const modal = document.querySelector(".modal");
+const container = document.querySelector(".container");
+const btnCancel = document.querySelector(".btn-cancel");
+const pMin = document.querySelector(".min");
+const pSec = document.querySelector(".sec");
+let timerFlag = false;
+let pause;
+let min = 9;
+let sec = 10;
 
-times.forEach(time => {
-  time.addEventListener("click", () => {
-    times.forEach(time => {
-      time.classList.remove("active");
+const addEvent = nodeList => {
+  nodeList.forEach(item => {
+    item.addEventListener("click", () => {
+      nodeList.forEach(item => {
+        item.classList.remove("active");
+      });
+      item.classList.add("active");
+      if (nodeList === times) {
+        min = item.dataset.min;
+        sec = item.dataset.sec;
+        pMin.textContent = min;
+        pSec.textContent = sec;
+      }
     });
-    time.classList.add("active");
-    console.log(time);
   });
+};
+const restart = nodeList => {
+  nodeList.forEach(item => {
+    item.classList.remove("active");
+  });
+};
+
+const timer = () => {
+  sec--;
+  if (sec <= 0) {
+    min--;
+    if (min <= 9) {
+      min = "0" + min;
+    }
+    sec = 59;
+  }
+
+  if (sec <= 9) {
+    sec = "0" + sec;
+  }
+  pMin.textContent = min;
+  pSec.textContent = sec;
+  console.log(min + ":" + sec);
+};
+
+addEvent(times);
+addEvent(environments);
+
+btnStart.addEventListener("click", e => {
+  pause = setInterval(timer, 1000);
+  modal.classList.add("active");
+  container.classList.add("blur");
+  timerFlag = true;
+});
+btnCancel.addEventListener("click", () => {
+  modal.classList.remove("active");
+  container.classList.remove("blur");
+  clearInterval(pause);
+  restart(times);
+  restart(environments);
 });
 
-environments.forEach(env => {
-  env.addEventListener("click", () => {
-    environments.forEach(env => {
-      env.classList.remove("active");
-    });
-    env.classList.add("active");
-    console.log(env);
-  });
+document.querySelector(".btn-pause").addEventListener("click", () => {
+  if (timerFlag === true) {
+    clearInterval(pause);
+    timerFlag = false;
+  } else {
+    pause = setInterval(timer, 1000);
+    timerFlag = true;
+  }
 });
